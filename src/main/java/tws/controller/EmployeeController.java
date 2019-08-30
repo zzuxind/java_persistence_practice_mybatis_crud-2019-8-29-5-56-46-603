018d8a10e2a4413bbe9f3be4f38b6c31837c731b
dingxin <zzuxind@163.com>
2019-08-30 13:45:58 +0800

    使用dozer进行转化

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -3,8 +3,8 @@ package tws.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tws.dto.EmployeeDTO;
 import tws.entity.Employee;
-import tws.repository.EmployeeMapper;
 import tws.service.EmployeeService;
 
 import java.net.URI;
@@ -15,18 +15,20 @@ import java.util.UUID;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    @Autowired
-    private EmployeeMapper employeeMapper;
+//    @Autowired
+//    private EmployeeMapper employeeMapper;
     @Autowired
     private EmployeeService employeeService;
 
     @GetMapping("")
-    public ResponseEntity<List<Employee>> getAll(@RequestParam(value = "nameLike",required = false) String nameLike,
-                                                 @RequestParam(value = "page",required = false) Integer page,
-                                                 @RequestParam(value = "pageSize",required = false)Integer pageSize) {
-        //Integer start=(page-1)*pageSize;
-        return ResponseEntity.ok(employeeMapper.selectAll(nameLike,page,pageSize));
+    public ResponseEntity<List<EmployeeDTO>> getAll(@RequestParam(value = "nameLike",required = false) String nameLike,
+                                                    @RequestParam(value = "page",required = false) Integer page,
+                                                    @RequestParam(value = "pageSize",required = false)Integer pageSize)
+    {
+
+        return ResponseEntity.ok(employeeService.selectAll(nameLike,page,pageSize));
     }
+
     @GetMapping("{employeeID}")
     public ResponseEntity getEmployWithDesc(@PathVariable String employeeID){
         return ResponseEntity.ok(employeeService.getEmployWithDesc(employeeID));
@@ -36,20 +38,20 @@ public class EmployeeController {
     public  ResponseEntity insert(@RequestBody Employee employee){
         String id= UUID.randomUUID().toString();
         employee.setId(id);
-        employeeMapper.insert(employee);
+        employeeService.insert(employee);
         return ResponseEntity.created(URI.create("employees/"+id)).build();
     }
 
     @PutMapping("{employeeID}")
     public ResponseEntity updateEmployeeByID(@PathVariable String employeeID,@RequestBody Employee employee){
-        employeeMapper.updateEmployeeByID(employeeID,employee);
+        employeeService.updateEmployeeByID(employeeID,employee);
         employee.setId(employeeID);
         return ResponseEntity.ok(employee);
     }
 
     @DeleteMapping("{employeeID}")
     public  ResponseEntity deleteEmployeeByID(@PathVariable String employeeID){
-        employeeMapper.deleteEmployeeByID(employeeID);
+        employeeService.deleteEmployeeByID(employeeID);
         return  ResponseEntity.ok().build();
     }
 
--- src/main/java/tws/service/EmployeeService.java
+++ src/main/java/tws/service/EmployeeService.java
@@ -1,15 +1,21 @@
 package tws.service;
 
+import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tws.dto.EmployeeDTO;
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EmployeeService {
     @Autowired
     private EmployeeMapper employeeMapper;
+
+    DozerBeanMapper dozerBeanMapper=new DozerBeanMapper();
 
 
     public EmployeeDTO getEmployWithDesc(String id){
@@ -23,9 +29,27 @@ public class EmployeeService {
         return employeeDTO;
     }
 
-//    public List<EmployeeDTO> selectAll(String nameLike) {
-//        List<EmployeeDTO> employeeDTOList=new ArrayList<>();
-//        List<Employee> employeeList=employeeMapper.selectAll(nameLike);
-//        employeeDTOList.addAll(employeeList);
-//    }
+    public List<EmployeeDTO> selectAll(String nameLike,Integer page,Integer pageSize) {
+        List<EmployeeDTO> employeeDTOList=new ArrayList<>();
+        List<Employee> employeeList=employeeMapper.selectAll(nameLike,page,pageSize);
+        for(Employee employee:employeeList){
+            EmployeeDTO employeeDTO=dozerBeanMapper.map(employee,EmployeeDTO.class);
+            String desc=String.format("name:%s,age:%s",employee.getName(),employee.getAge());
+            employeeDTO.setDesc(desc);
+            employeeDTOList.add(employeeDTO);
+        }
+        return employeeDTOList;
+    }
+
+    public void insert(Employee employee) {
+        employeeMapper.insert(employee);
+    }
+
+    public void updateEmployeeByID(String employeeID, Employee employee) {
+        employeeMapper.updateEmployeeByID(employeeID,employee);
+    }
+
+    public void deleteEmployeeByID(String employeeID) {
+        employeeMapper.deleteEmployeeByID(employeeID);
+    }
 }
